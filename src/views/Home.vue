b2247fbd74c1070cdde0e26de16abaaf38ed8062
Dallan Adamson <dallanadamson@gmail.com>
2021-11-06 15:22:58 -0600

    hey

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex flex-col">
+	<h1 class="text-4xl py-4 font-bold">Welcome to all nerds!</h1>
+	<h2 class="text-2xl font-semibold py-4">Start building your star wars team deck here with our custom deck builder</h2>
+	<img src="../assets/vader.jpg" class="w-1/2 mx-auto">
+	<h2 class="text-2xl py-2 font-semibold py-4">Build your strongest team with Lord Vader</h2>
+	<img src="../assets/gangGang2.jpg" class="w-1/2 mx-auto">
+	<h2 class="text-2xl py-2 font-semibold py-4">Pown some newbs with your gang</h2>
+	<img src="../assets/darkCav.jpg" class="w-1/2 mx-auto">
+	<h2 class="text-2xl py-2 font-semibold py-4">Hide your deepest secrets in the caverns of strange planets</h2>
+	<img src="../assets/helmet.jpg" class="w-1/2 mx-auto">
+	<h2 class="text-2xl py-2 font-semibold py-4">Become the most feared leader in the known galaxy</h2>
+	<router-link class="w-1/3 h-12 my-4 mx-auto" to="/teamselect"><button class="border-2 h-full w-full rounded-md bg-green-300">Start Building Your Team Here!</button></router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
