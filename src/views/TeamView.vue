e0eaece1947d70afe75a3944646013df45f6a32e
Dallan Adamson <dallanadamson@gmail.com>
2021-11-06 16:14:44 -0600

    all done

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,26 +1,71 @@
 <template>
   <div class="teamselect">
-    <h1>This is a team viewing page</h1>
-    <p v-if="noTeamMembers">No team members have been chosen.</p>
-    <p v-if="noPlanetChosen">No planet has been chosen.</p>
-    <p v-if="noShipChosen">No ship has been chosen.</p>
-    <div class="wrapper">
-      <div class="people">
-        <div class="person" v-for="person in team" :key="person.id">
-          <div class="info">
-          <h1>{{person.name}}</h1>
-          <p>Power: {{person.power}}</p>
-          <p>Health: {{person.health}}</p>
-        </div>
-        <div class="image">
-          <img :src="person.picture">
-        </div>
-        <div class="stats">
-          <button @click="removeFromTeam(person.id)" class="center">Remove From Team</button>
-        </div>
-      </div>
-    </div>
-    </div>
+    <h1 class="text-4xl pb-8">View your Team!</h1>
+	<div class="teamDisplay">
+		<h1 class="teamSection">Team Members:</h1>
+		<div v-if="noTeamMembers">
+			<img src="https://st.depositphotos.com/2142621/2870/i/600/depositphotos_28708655-stock-photo-oh-no.jpg">
+			<p class="py-4 text-xl text-red-500">OH NO! You haven't selected any team members!</p>
+		</div>
+		<div class="wrapper">
+			<div class="people">
+				<div class="person" v-for="person in team" :key="person.id">
+					<div class="info">
+						<h1>{{person.name}}</h1>
+						<p>Power: {{person.power}}</p>
+						<p>Health: {{person.health}}</p>
+					</div>
+					<div class="image">
+						<img :src="person.picture">
+					</div>
+					<div class="stats">
+						<button @click="removeFromTeam(person.id)" class="center">Remove From Team</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<h1 class="teamSection">Ship Chosen:</h1>
+		<div v-if="noShipChosen">
+			<img src="https://st.depositphotos.com/2142621/2870/i/600/depositphotos_28708655-stock-photo-oh-no.jpg">
+			<p class="py-4 text-xl text-red-500">Failure! You haven't selected a freaking ship!</p>
+		</div>
+		<div class="wrapper">
+			<div class="person" v-if="!noShipChosen">
+				<div class="shipInfo">
+					<h1 class="text-lg">{{ship.name}}</h1>
+					<p class="shipP">Health: {{ship.health}}</p>
+					<p class="shipP">Damage: {{ship.damage}}</p>
+					<p class="shipP">Mobility: {{ship.mobility}}</p>
+				</div>
+				<div class="image">
+					<img :src="ship.picture">
+				</div>
+				<div class="stats">
+					<button @click="removeShip()" class="center">Remove Ship</button>
+				</div>
+			</div>
+		</div>
+		<h1 class="teamSection">Planet Selected:</h1>
+		<div v-if="noPlanetChosen">
+			<img src="https://st.depositphotos.com/2142621/2870/i/600/depositphotos_28708655-stock-photo-oh-no.jpg">
+			<p class="py-4 text-xl text-red-500">Noooo! You haven't selected a planet you BOZO!</p>
+		</div>
+		<div class="wrapper">
+			<div class="person" v-if="!noPlanetChosen">
+				<div class="info-planet">
+					<h1 class="text-lg">{{planet.name}}</h1>
+					<p class="shipP">Resources: {{planet.resources}}</p>
+					<p class="shipP">Danger: {{planet.danger}}</p>
+				</div>
+				<div class="image">
+					<img :src="planet.picture">
+				</div>
+				<div class="stats">
+					<button @click="removePlanet()" class="center">Remove Planet</button>
+				</div>
+			</div>
+		</div>
+	</div>
   </div>
 </template>
 
@@ -75,10 +120,27 @@ export default {
 </script>
 
 <style scoped>
+
+.teamSection {
+	font-size: 30px;
+	padding-bottom: 15px;
+}
+
+.teamDisplay {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
 .wrapper {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.shipP {
+	font-size: 10px;
 }
 
 .people {
@@ -112,6 +174,13 @@ export default {
   color: #000;
   padding: 10px 30px;
   height: 80px;
+}
+
+.shipInfo {
+	background: #f21d1d;
+	color: #000;
+	padding: 10px 30px;
+	height: 80px;
 }
 
 .info-planet {
